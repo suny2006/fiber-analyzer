a3a02897ca1396dbacc2a3852143b1b5b2d02a31
suny2006 <zediemaster@naver.com>
2026-06-07 08:24:18 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'fiber-analyzer-v3-1';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))); });
